3cb9b628b8ede98a4fbf6d8c837cf350a8af4938
mapy1874 <pm@unc.edu>
2019-12-04 16:24:51 -0500

    nothing changes

--- src/a8/GameOfLife.java
+++ src/a8/GameOfLife.java
@@ -3,7 +3,7 @@ package a8;
 import game_of_life.Main;
 
 public class GameOfLife {
-	  // for calling the real game
+	  // for calling the real game.
 	  public static void main(String[] args) {
 		  Main.main(null);
 	  }
